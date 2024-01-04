389589dff0062a8a973e1be5957dc38bea2ba286
bogdan.marcu <bogdanmarcu2002@icloud.com>
2024-01-04 19:01:23 +0200

    Create Package

--- Models/Package.cs
+++ Models/Package.cs
@@ -5,6 +5,7 @@ namespace PostApplication.Models
     public class Package
     {
         public int ID { get; set; }
+
         [Display(Name = "Weight")]
         public double Weight { get; set; }
 
@@ -12,12 +13,15 @@ namespace PostApplication.Models
         public string Status { get; set; }
 
         [Display(Name = "Sender")]
+        public int SenderId { get; set; }
         public User Sender { get; set; }
 
         [Display(Name = "Receiver")]
+        public int ReceiverId { get; set; }
         public User Receiver { get; set; }
 
         [Display(Name = "Courier")]
+        public int AssignedCourierId { get; set; }
         public Courier AssignedCourier { get; set; }
     }
 }

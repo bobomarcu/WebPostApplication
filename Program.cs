6fe2f656a856c2414fa957a189ba9b8fa6468d86
bogdan.marcu <bogdanmarcu2002@icloud.com>
2024-01-07 01:11:17 +0200

    Add auth

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PostApplication.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+
 builder.Services.AddDbContext<PostApplicationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PostApplicationContext") ?? throw new InvalidOperationException("Connection string 'PostApplicationContext' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("PostApplicationContext") ?? throw new InvalidOperationException("Connectionstring 'PostApplicationContext' not found.")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+ .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
